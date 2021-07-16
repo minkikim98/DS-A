958783b4ee1ea16df8c6ecaa34ab5572207b67ff
minkikim98 <minkikim98@gmail.com>
2021-07-16 16:31:58 -0700

    added binaryTreeLevelOrder

---
+++ w-Of-7-11-2021/7-16-2021/binaryTreeLevelOrder.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+// 7/16/2021
+
+const levelOrder = function (root) {
+    if (!root) return [];
+    const Q = [root, -1];
+    const ordered = [];
+
+    let temp = [];
+
+    while (Q.length > 0) {
+        const currNode = Q.shift();
+
+        if (currNode === -1) {
+            ordered.push(temp);
+            temp = [];
+            if (Q.length > 0) Q.push(-1);
+            continue;
+        }
+
+        temp.push(currNode.val);
+
+        if (currNode.left) Q.push(currNode.left);
+        if (currNode.right) Q.push(currNode.right);
+    }
+    return ordered;
+};
