866e61417dc60c7ea771be03fa30ddb6ee7403cc
minkikim98 <minkikim98@gmail.com>
2021-07-22 10:09:58 -0700

    added alternate solution for SpiralMatrix

--- w-Of-7-18-2021/7-22-2021/SpiralMatrix.js
+++ w-Of-7-18-2021/7-22-2021/SpiralMatrix.js
@@ -1,7 +1,6 @@
 // https://leetcode.com/explore/interview/card/top-interview-questions-hard/116/array-and-strings/828
 
 // Solution heavily inspired by qwl5000's solution.
-
 /**
  * @param {number[][]} matrix
  * @return {number[]}
@@ -37,3 +36,36 @@ const spiralOrder = function (matrix) {
 
     return res;
 };
+
+
+// This version recommended by GreatLim, to check duplicates
+const spiralOrder = (matrix) => {
+    const res = [];
+
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+    const totalElements = numRows * numCols;
+
+    let rowBegin = 0, rowEnd = numRows - 1, colBegin = 0, colEnd = numCols - 1;
+
+
+    while (res.length < totalElements) {
+        for (let i = colBegin; i <= colEnd && res.length < totalElements; i++)
+            res.push(matrix[rowBegin][i]);
+        rowBegin++;
+
+        for (let i = rowBegin; i <= rowEnd && res.length < totalElements; i++)
+            res.push(matrix[i][colEnd]);
+        colEnd--;
+
+        for (let i = colEnd; i >= colBegin && res.length < totalElements; i--)
+            res.push(matrix[rowEnd][i]);
+        rowEnd--;
+
+        for (let i = rowEnd; i >= rowBegin && res.length < totalElements; i--)
+            res.push(matrix[i][colBegin])
+        colBegin++;
+    }
+
+    return res;
+}
