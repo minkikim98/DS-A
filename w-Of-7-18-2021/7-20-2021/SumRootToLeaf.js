39c6beb013b4d2f22c6b2d64959aeb9f0fa8f9d4
minkikim98 <minkikim98@gmail.com>
2021-07-23 11:35:55 -0700

    syntax simplification for SumRootToLeaf

--- w-Of-7-18-2021/7-20-2021/SumRootToLeaf.js
+++ w-Of-7-18-2021/7-20-2021/SumRootToLeaf.js
@@ -1,23 +1,22 @@
 // https://leetcode.com/problems/sum-root-to-leaf-numbers/
 // 7/20/2021
 
-const sumNumbers = function (root) {
-    if (!root) return 0;
-
-    let allNumbers = [];
-    sumHelper(root, 0, allNumbers);
-
-    return allNumbers.reduce((acc, curr) => acc + curr);
+const sumNumbers = function(root) {
+	if (!root) return 0;
+    
+    const allNumbers = [];
+   	sumHelper(root, 0, allNumbers);
+    
+	return allNumbers.reduce((acc, curr) => acc + curr);
 };
 
 const sumHelper = (root, currNum, allNumbers) => {
-    if (!root.left && !root.right) {
-        allNumbers.push(currNum * 10 + root.val);
-    }
-    if (root.left) {
+	if (!root.left && !root.right) 
+		allNumbers.push(currNum * 10 + root.val);
+
+    if (root.left) 
         sumHelper(root.left, currNum * 10 + root.val, allNumbers);
-    }
-    if (root.right) {
+
+    if (root.right) 
         sumHelper(root.right, currNum * 10 + root.val, allNumbers);
-    }
 }
