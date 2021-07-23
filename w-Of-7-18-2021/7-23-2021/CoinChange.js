d1378acec5687b085b57f29abb1946982d081b54
minkikim98 <minkikim98@gmail.com>
2021-07-23 11:12:25 -0700

    added CoinChange

---
+++ w-Of-7-18-2021/7-23-2021/CoinChange.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/111/dynamic-programming/809/
+// Solution inspired by wyattliu
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+
+const maxVal = Number.POSITIVE_INFINITY;
+
+const coinChange = function (coins, amount) {
+    const memo = new Array(amount + 1).fill(0);
+    coinHelper(coins, amount, memo);
+    return memo[amount];
+};
+
+const coinHelper = (coins, rem, memo) => {
+    if (rem < 0) return -1;
+    if (rem === 0) return 0;
+    if (memo[rem] !== 0) return memo[rem];
+
+    let min = maxVal;
+    for (const coin of coins) {
+        const res = coinHelper(coins, rem - coin, memo);
+        if (res < min && res >= 0)
+            min = res + 1;
+    }
+
+    memo[rem] = min === maxVal ? -1 : min;
+    return memo[rem];
+}
