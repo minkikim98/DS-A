24dbf7e7c143de26bf85fe60f0bd284606b83fec
minkikim98 <minkikim98@gmail.com>
2021-07-29 16:35:22 -0700

    added alternate solution with inner join for EmployeesEarningMore

--- w-Of-7-25-2021/EmployeesEarningMore.sql
+++ w-Of-7-25-2021/EmployeesEarningMore.sql
@@ -1,6 +1,14 @@
 -- https://leetcode.com/problems/employees-earning-more-than-their-managers/
 -- Solution inspired by hsnu112305
 
+-- Original
 select e.Name as Employee
 from Employee as e, Employee as m
 where e.ManagerId = m.id and e.Salary > m.salary
+
+-- Inner Join (slower, inspired by mzchen)
+select e.name as Employee
+from Employee as e
+inner join Employee as m
+on e.ManagerId = m.Id
+where e.Salary > m.Salary
